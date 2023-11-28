ca8ff9aaf02d1e35b43673642c5e3048a960ec01
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2023-11-28 11:12:48 -0300

    Cap. 02 - Escutando eventos

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -12,15 +12,21 @@ const Formulario = () => {
     "UX e Design",
     "Mobile",
     "Inovação e Gestão",
-  ];
+  ]
+
+  const aoSalvar = (event) => {
+    event.preventDefault()
+    console.log('Formulario foi sumetido')
+  }
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa label="Time" itens={times} />
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
         <Botao>
           Criar Card
         </Botao>

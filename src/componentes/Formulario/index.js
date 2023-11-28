fb80427f5f90ec57603c6d7994fd2406a67d1d15
Rodolfo Ghiggi <rodolfo@maxiprod.com.br>
2023-11-28 10:11:12 -0300

    Cap. 02 - Lista dos times

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,15 +1,28 @@
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
-const Formulario = () => (
-  <section className="formulario">
-    <form>
-      <h2>Preencha os dados para criar o card do colaborador</h2>
-      <CampoTexto label="Nome" placeholder="Digite seu nome" />
-      <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-    </form>
-  </section>
-);
+const Formulario = () => {
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
+  return (
+    <section className="formulario">
+      <form>
+        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <CampoTexto label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa label="Time" itens={times} />
+      </form>
+    </section>
+  );
+};
 
 export default Formulario;

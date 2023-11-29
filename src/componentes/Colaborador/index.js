255ffc6855a1e612c315723ea854f78a9ae3749f
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2023-11-29 09:15:44 -0300

    Cap. 04 - Card de Colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = (props) => {
+    return(
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/rodolfoghi.png' alt='Foto do Rodolfo Ghiggi' />
+            </div>
+            <div className='rodape'>
+                <h4>Rodolfo Ghiggi</h4>
+                <h5>Team Leader</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
